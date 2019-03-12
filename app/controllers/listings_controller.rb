24a4bef08085d56167882871aa23e7307a6e6b2d
Jennifer Lynch <jl.lynch9@gmail.com>
2019-03-12 18:29:02 +0100

    final commit for the day

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,9 +1,13 @@
 class ListingsController < ApplicationController
   def index
     @listings = Listing.all
+    @title = 'MY HOMES'
   end
 
   def show
     @listing = Listing.find(params[:id])
+    @title = 'MY PROPERTY'
   end
+
+
 end
