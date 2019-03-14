cfa7fcc575ae749df08fa7c40e01e9c461a0ac86
Andre Leroy Beaulieu <andrechi14@gmail.com>
2019-03-14 11:11:33 +0100

    add routes, links in navbar and footer, create profile page, create inbox page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+  get 'messages/show'
   devise_for :users
   root to: 'listings#index'
   get "/faq" => "pages#faq"
@@ -10,36 +12,3 @@ Rails.application.routes.draw do
   resources :messages, only: [ :index, :show ]
 
 end
-
-
-# Prefix Verb   URI Pattern                                                                              Controller#Action
-#          new_user_session GET    /users/sign_in(.:format)                                                                 devise/sessions#new
-#              user_session POST   /users/sign_in(.:format)                                                                 devise/sessions#create
-#      destroy_user_session DELETE /users/sign_out(.:format)                                                                devise/sessions#destroy
-#         new_user_password GET    /users/password/new(.:format)                                                            devise/passwords#new
-#        edit_user_password GET    /users/password/edit(.:format)                                                           devise/passwords#edit
-#             user_password PATCH  /users/password(.:format)                                                                devise/passwords#update
-#                           PUT    /users/password(.:format)                                                                devise/passwords#update
-#                           POST   /users/password(.:format)                                                                devise/passwords#create
-#  cancel_user_registration GET    /users/cancel(.:format)                                                                  devise/registrations#cancel
-#     new_user_registration GET    /users/sign_up(.:format)                                                                 devise/registrations#new
-#    edit_user_registration GET    /users/edit(.:format)                                                                    devise/registrations#edit
-#         user_registration PATCH  /users(.:format)                                                                         devise/registrations#update
-#                           PUT    /users(.:format)                                                                         devise/registrations#update
-#                           DELETE /users(.:format)                                                                         devise/registrations#destroy
-#                           POST   /users(.:format)                                                                         devise/registrations#create
-#                      root GET    /                                                                                        listings#index
-#                       faq GET    /faq(.:format)                                                                           pages#faq
-#                      user GET    /users/:id(.:format)                                                                     users#show
-#            listing_photos GET    /listings/:listing_id/photos(.:format)                                                   photos#index
-#                  listings GET    /listings(.:format)                                                                      listings#index
-#                   listing GET    /listings/:id(.:format)                                                                  listings#show
-#                     photo PATCH  /photos/:id(.:format)                                                                    photos#update
-#                           PUT    /photos/:id(.:format)                                                                    photos#update
-#                  messages GET    /messages(.:format)                                                                      messages#index
-#                   message GET    /messages/:id(.:format)                                                                  messages#show
-#        rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
-# rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
-#        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
-# update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
-#      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)
