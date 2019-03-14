5561d2b340882f8c5c90a7d9d2332d0d494cc048
Andre Leroy Beaulieu <andrechi14@gmail.com>
2019-03-14 13:02:22 +0100

    profile page done for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
   def show
     @title = "PROFILE"
   end
+
+  def update
+    current_user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:photo)
+  end
 end
