dabeab3666fb596d20aa4f48c93431f14a323e96
Jennifer Lynch <jl.lynch9@gmail.com>
2019-03-14 17:06:11 +0100

    pulled origin master

--- app/javascript/packs/inbox_filter.js
+++ app/javascript/packs/inbox_filter.js
@@ -1,1 +1,6 @@
-inbox_filter.js
+
+const fliterBar = document.querySelector('.filter-bar')
+
+filterBar.addEventListener('click', (event) => {
+  event.currentTarget.classList.toggle("inbox-filter-menu");
+}
