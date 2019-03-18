de441d3fb57eb1ec6bff523a61d7b162725ae015
Jennifer Lynch <jl.lynch9@gmail.com>
2019-03-18 18:37:28 +0100

    filter working, tabs working, good times!

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,25 @@
 class MessagesController < ApplicationController
   def index
     @title = 'INBOX'
-    @messages = Message.all
+    # this will change to ModelName.where(@user = current_user)
     @listings = Listing.all
+
+    # I need to filter by doc_type(invoice, contract, other) ..
+    if params[:doc_type]
+      @messages = Message.where(doc_type: params[:doc_type])
+    else
+      @messages = Message.all
+    end
+    # # and  listing(listing name1, 2, 3) ...
+    if params[:listing_id]
+      @messages = @messages.where(listing_id: params[:listing_id])
+    else
+      @messages
+    end
   end
 
   def show
-    @title =  'MESSAGE'
+    @title = 'MESSAGE'
     @message = Message.find(params[:id])
   end
 end
