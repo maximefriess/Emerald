1d3bdad46593388f9456b8ec5c6f6d49a9e5364c
Jennifer Lynch <jl.lynch9@gmail.com>
2019-03-19 14:39:35 +0100

    added filter to analytics

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,23 @@
 class BookingsController < ApplicationController
   def index
     @title = 'ANALYTICS'
-    @listing = Listing.find(params[:listing_id])
+    # @listing = Listing.find(params[:listing_id])
+    @listings = Listing.all
     # @booking_calculations = BookingCalculations.new(listing: params[:listing], month: params[:month])
-
+    if params[:listing_id]
+      @bookings = @bookings.where(listing_id: params[:listing_id])
+    else
+      @bookings
+    end
+    if params[:month]
+      @bookings = @bookings.where(month: params[:month])
+    else
+      @bookings = Booking.all
+    end
+    if params[:year]
+      @bookings = @bookings.where(year: params[:year])
+    else
+      @bookings
+    end
   end
-
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import { backArrow } from './back_function';
 import { inboxFilter } from './inbox_filter';
+import { analyticsFilter } from './analytics_filter';
 
 backArrow();
 inboxFilter();
+analyticsFilter();
