c665ea46917655ede097365129c6daf18a350751
Maxime Friess <maxime@emeraldstay.com>
2019-03-20 16:29:46 +0100

    bug fixes for pdf thumbnail

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@ import { backArrow } from './back_function';
 import { inboxFilter } from './inbox_filter';
 import { analyticsFilter } from './analytics_filter';
 import { analyticsFilterShow } from './analytics_filter_show';
-import "d3graph"
+// import "d3graph"
 
 backArrow();
 inboxFilter();
