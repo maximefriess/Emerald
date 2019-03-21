2b145697ae757dfe0f817fb2119e1bb0c1c95c9b
Maxime Friess <maxime@emeraldstay.com>
2019-03-21 10:06:59 +0100

    small fixes

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -2,7 +2,7 @@ class DocumentUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   version :thumb do
-    process :resize_to_fit => [345, 245]
+    process :resize_to_fit => [495, 350]
     process :convert => 'png'
   end
   # Remove everything else
